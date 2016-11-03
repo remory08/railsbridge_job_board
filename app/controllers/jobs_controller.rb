5b554fd97185ca77caededa8b3e3661704cdebbd
Ryne Emory <ryneemory@gmail.com>
2016-11-03 16:57:47 -0600

    initial commit

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,15 @@
+class JobsController < ApplicationController
+  def show
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
